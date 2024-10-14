164c214198d0e97215a1e4cd55eb9516943278dd
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-14 20:03:05 -0300

    plasa: add plasa main contract

--- src/plasa/Plasa.sol
+++ src/plasa/Plasa.sol
@@ -1,58 +1,74 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "../spaces/Space.sol";
-// import "../stamps/AccountOwnershipStamp.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../spaces/Space.sol";
+import "../stamps/AccountOwnershipStamp.sol";
+import "../spaces/interfaces/ISpace.sol";
+import "../stamps/interfaces/IAccountOwnershipStamp.sol";
 
-// /// @title Plasa - The main contract for managing spaces and account ownership stamps
-// /// @notice This contract serves as the central hub for all Plasa-related contracts
-// /// @dev Inherits from Ownable for access control
-// contract Plasa is Ownable {
-//     /// @notice Array to store all created spaces
-//     Space[] public spaces;
+/// @title Plasa - The main contract for managing spaces and account ownership stamps
+/// @notice This contract serves as the central hub for all Plasa-related contracts
+/// @dev Inherits from Ownable for access control
+contract Plasa is Ownable {
+	/// @notice Array to store all created spaces
+	ISpace[] public spaces;
 
-//     /// @notice Mapping to store account ownership stamps by platform name
-//     mapping(string => AccountOwnershipStamp) public accountOwnershipStamps;
+	/// @notice Mapping to store account ownership stamps by platform name
+	mapping(string => IAccountOwnershipStamp) public accountOwnershipStamps;
 
-//     /// @notice Event emitted when a new space is created
-//     event SpaceCreated(address spaceAddress, address owner);
+	/// @notice Event emitted when a new space is created
+	event SpaceCreated(address spaceAddress, address owner);
 
-//     /// @notice Event emitted when a new account ownership stamp is created
-//     event AccountOwnershipStampCreated(string platform, address stampAddress);
+	/// @notice Event emitted when a new account ownership stamp is created
+	event AccountOwnershipStampCreated(string platform, address stampAddress);
 
-//     /// @notice Initializes the Plasa contract
-//     /// @param initialOwner The address that will own this Plasa contract
-//     constructor(address initialOwner) Ownable(initialOwner) {}
+	/// @notice Custom error for when a stamp already exists for a platform
+	error StampAlreadyExists(string platform);
 
-//     /// @notice Creates a new space
-//     /// @param stampSigner The address authorized to sign mint requests for follower stamps
-//     /// @param platform The platform name (e.g., "Instagram", "Twitter")
-//     /// @param followed The account being followed
-//     /// @param spaceName The name of the space
-//     /// @param spaceDescription The description of the space
-//     /// @param spaceImageUrl The URL of the space's image
-//     /// @return The address of the newly created space
-//     function createSpace(
-//         address stampSigner,
-//         string memory platform,
-//         string memory followed,
-//         string memory spaceName,
-//         string memory spaceDescription,
-//         string memory spaceImageUrl
-//     ) external returns (address) {
-//         Space newSpace = new Space(
-//             msg.sender,
-//             stampSigner,
-//             platform,
-//             followed,
-//             spaceName,
-//             spaceDescription,
-//             spaceImageUrl
-//         );
-//         spaces.push(newSpace);
-//         emit SpaceCreated(address(newSpace), msg.sender);
+	/// @notice Initializes the Plasa contract
+	/// @param initialOwner The address that will own this Plasa contract
+	constructor(address initialOwner) Ownable(initialOwner) {}
 
-//     /// @notice Creates a new account ownership stamp for a platform
-//     /// @param
-// }
+	/// @notice Creates a new space
+	/// @param stampSigner The address authorized to sign mint requests for follower stamps
+	/// @param platform The platform name (e.g., "Instagram", "Twitter")
+	/// @param followed The account being followed
+	/// @param spaceName The name of the space
+	/// @param spaceDescription The description of the space
+	/// @param spaceImageUrl The URL of the space's image
+	/// @return The address of the newly created space
+	function createSpace(
+		address stampSigner,
+		string memory platform,
+		string memory followed,
+		string memory spaceName,
+		string memory spaceDescription,
+		string memory spaceImageUrl
+	) external returns (address) {
+		ISpace newSpace = ISpace(
+			new Space(msg.sender, stampSigner, platform, followed, spaceName, spaceDescription, spaceImageUrl)
+		);
+		spaces.push(newSpace);
+		emit SpaceCreated(address(newSpace), msg.sender);
+		return address(newSpace);
+	}
+
+	/// @notice Creates a new account ownership stamp
+	/// @param platform The platform name (e.g., "Instagram", "Twitter")
+	/// @param stampSigner The address authorized to sign ownership verification requests
+	/// @return The address of the newly created account ownership stamp
+	function createAccountOwnershipStamp(
+		string memory platform,
+		address stampSigner
+	) external onlyOwner returns (address) {
+		if (address(accountOwnershipStamps[platform]) != address(0)) {
+			revert StampAlreadyExists(platform);
+		}
+
+		IAccountOwnershipStamp newStamp = new AccountOwnershipStamp(stampSigner, platform);
+		accountOwnershipStamps[platform] = newStamp;
+		emit AccountOwnershipStampCreated(platform, address(newStamp));
+		return address(newStamp);
+	}
+}

ead31a1349ccefb08527c282c19be13e31569046
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-14 21:50:40 -0300

    space: remove ownable

--- src/spaces/Space.sol
+++ src/spaces/Space.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/ISpace.sol";
 import "../stamps/interfaces/IFollowerSinceStamp.sol";
 import "../points/interfaces/IFollowerSincePoints.sol";

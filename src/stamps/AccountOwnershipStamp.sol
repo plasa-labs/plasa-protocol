5b5f8fc3f4791d3e9994324b7ef1fbde2af6dc9a
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-15 19:49:02 -0300

    style: update imports

--- src/stamps/AccountOwnershipStamp.sol
+++ src/stamps/AccountOwnershipStamp.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Stamp } from "./Stamp.sol";
+import { Stamp, IStampView } from "./Stamp.sol";
 import { IAccountOwnershipStamp } from "./interfaces/IAccountOwnershipStamp.sol";
-import { IStampView } from "./interfaces/IStampView.sol";
 
 /// @title AccountOwnershipStamp
 /// @notice A contract for minting and managing account ownership stamps on specific platforms
--- src/stamps/FollowerSinceStamp.sol
+++ src/stamps/FollowerSinceStamp.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Stamp } from "./Stamp.sol";
+import { Stamp, IStampView } from "./Stamp.sol";
 import { IFollowerSinceStamp } from "./interfaces/IFollowerSinceStamp.sol";
-import { IStampView } from "./interfaces/IStampView.sol";
 
 /// @title FollowerSinceStamp
 /// @notice A contract for minting NFTs that represent a user's follower status since a specific date

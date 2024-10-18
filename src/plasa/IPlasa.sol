ba63e4af17f1cfd6458aa822f49e3bdabb7ec860
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-18 19:04:53 -0300

    update plasa

---
+++ src/plasa/IPlasa.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { ISpace } from "../spaces/interfaces/ISpace.sol";
+import { IStamp } from "../stamps/interfaces/IStamp.sol";
+import { IPlasaView } from "./IPlasaView.sol";
+
+/// @title IPlasa - Interface for the main Plasa contract
+/// @notice This interface defines the core functionality for managing spaces and stamps in the Plasa ecosystem
+/// @dev Implements IPlasaView for read-only operations
+interface IPlasa is IPlasaView {
+	/// @notice Retrieves all created spaces
+	/// @return An array of ISpace interfaces representing all spaces
+	function getSpaces() external view returns (ISpace[] memory);
+
+	/// @notice Retrieves a specific space by its index
+	/// @param index The index of the space to retrieve
+	/// @return The ISpace interface of the requested space
+	function getSpace(uint256 index) external view returns (ISpace);
+
+	/// @notice Retrieves all stamps
+	/// @return An array of IStamp interfaces representing all stamps
+	function getStamps() external view returns (IStamp[] memory);
+
+	/// @notice Retrieves a specific stamp by its index
+	/// @param index The index of the stamp to retrieve
+	/// @return The IStamp interface of the requested stamp
+	function getStamp(uint256 index) external view returns (IStamp);
+
+	/// @notice Adds a new stamp to the Plasa ecosystem
+	/// @dev Only callable by authorized entities
+	/// @param stamp The address of the stamp contract to add
+	function addStamp(address stamp) external;
+
+	/// @notice Adds a new space to the Plasa ecosystem
+	/// @dev Only callable by authorized entities
+	/// @param space The address of the space contract to add
+	function addSpace(address space) external;
+}
--- src/plasa/Plasa.sol
+++ src/plasa/Plasa.sol
@@ -4,108 +4,34 @@ pragma solidity ^0.8.20;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { Space, ISpace, ISpaceView } from "../spaces/Space.sol";
 import { AccountOwnershipStamp, IAccountOwnershipStamp } from "../stamps/AccountOwnershipStamp.sol";
-import { IPlasaView } from "./IPlasaView.sol";
+import { IPlasa } from "./IPlasa.sol";
 import { IStamp, IStampView } from "../stamps/interfaces/IStamp.sol";
+import { IPlasaView } from "./IPlasaView.sol";
 
 /// @title Plasa - The main contract for managing spaces and account ownership stamps
-/// @notice This contract serves as the central hub for all Plasa-related contracts
-/// @dev Inherits from Ownable for access control
-contract Plasa is Ownable, IPlasaView {
+/// @notice This contract serves as the central hub for all Plasa-related operations
+/// @dev Inherits from Ownable for access control and implements IPlasa interface
+contract Plasa is Ownable, IPlasa {
 	/// @notice Array to store all created spaces
-	ISpace[] public spaces;
+	ISpace[] private _spaces;
 
-	IStamp[] public stamps;
-
-	/// @notice Mapping to store account ownership stamps by platform name
-	mapping(string => IAccountOwnershipStamp) public accountOwnershipStamps;
-
-	/// @notice Event emitted when a new space is created
-	event SpaceCreated(address spaceAddress, address owner);
-
-	/// @notice Event emitted when a new account ownership stamp is created
-	event AccountOwnershipStampCreated(string platform, address stampAddress);
-
-	/// @notice Custom error for when a stamp already exists for a platform
-	error StampAlreadyExists(string platform);
+	/// @notice Array to store all stamps
+	IStamp[] private _stamps;
 
 	/// @notice Initializes the Plasa contract
 	/// @param initialOwner The address that will own this Plasa contract
 	constructor(address initialOwner) Ownable(initialOwner) {}
 
-	/// @notice Creates a new space
-	/// @param initialSuperAdmins An array of addresses to be granted the SUPER_ADMIN_ROLE
-	/// @param initialAdmins An array of addresses to be granted the ADMIN_ROLE
-	/// @param initialModerators An array of addresses to be granted the MODERATOR_ROLE
-	/// @param stampSigner The address authorized to sign mint requests for follower stamps
-	/// @param platform The platform name (e.g., "Instagram", "Twitter")
-	/// @param followed The account being followed
-	/// @param spaceName The name of the space
-	/// @param spaceDescription The description of the space
-	/// @param spaceImageUrl The URL of the space's image
-	/// @param pointsSymbol The symbol of the points
-	/// @param minPointsToAddOpenQuestionOption The minimum points required to add an open question option
-	/// @return The address of the newly created space
-	function createSpace(
-		address[] memory initialSuperAdmins,
-		address[] memory initialAdmins,
-		address[] memory initialModerators,
-		address stampSigner,
-		string memory platform,
-		string memory followed,
-		string memory spaceName,
-		string memory spaceDescription,
-		string memory spaceImageUrl,
-		string memory pointsSymbol,
-		uint256 minPointsToAddOpenQuestionOption
-	) external returns (address) {
-		ISpace newSpace = new Space(
-			initialSuperAdmins,
-			initialAdmins,
-			initialModerators,
-			stampSigner,
-			platform,
-			followed,
-			spaceName,
-			spaceDescription,
-			spaceImageUrl,
-			pointsSymbol,
-			minPointsToAddOpenQuestionOption
-		);
-		spaces.push(newSpace);
-		emit SpaceCreated(address(newSpace), msg.sender);
-		return address(newSpace);
-	}
-
-	/// @notice Creates a new account ownership stamp
-	/// @param platform The platform name (e.g., "Instagram", "Twitter")
-	/// @param stampSigner The address authorized to sign ownership verification requests
-	/// @return The address of the newly created account ownership stamp
-	function createAccountOwnershipStamp(
-		string memory platform,
-		address stampSigner
-	) external onlyOwner returns (address) {
-		if (address(accountOwnershipStamps[platform]) != address(0)) {
-			revert StampAlreadyExists(platform);
-		}
-
-		IAccountOwnershipStamp newStamp = new AccountOwnershipStamp(address(this), stampSigner, platform);
-		accountOwnershipStamps[platform] = newStamp;
-		emit AccountOwnershipStampCreated(platform, address(newStamp));
-		return address(newStamp);
-	}
-
-	/// @notice Retrieves the Plasa view for a given user
-	/// @param user The address of the user to get the Plasa view for
-	/// @return The Plasa view for the given user
+	/// @inheritdoc IPlasaView
 	function getPlasaView(address user) external view returns (IPlasaView.PlasaView memory) {
-		IStampView.StampView[] memory stampsViews = new IStampView.StampView[](stamps.length);
-		for (uint256 i = 0; i < stamps.length; i++) {
-			stampsViews[i] = stamps[i].getStampView(user);
+		IStampView.StampView[] memory stampsViews = new IStampView.StampView[](_stamps.length);
+		for (uint256 i = 0; i < _stamps.length; i++) {
+			stampsViews[i] = _stamps[i].getStampView(user);
 		}
 
-		ISpaceView.SpacePreview[] memory spacesPreviews = new ISpaceView.SpacePreview[](spaces.length);
-		for (uint256 i = 0; i < spaces.length; i++) {
-			spacesPreviews[i] = spaces[i].getSpacePreview(user);
+		ISpaceView.SpacePreview[] memory spacesPreviews = new ISpaceView.SpacePreview[](_spaces.length);
+		for (uint256 i = 0; i < _spaces.length; i++) {
+			spacesPreviews[i] = _spaces[i].getSpacePreview(user);
 		}
 
 		IPlasaView.PlasaData memory data = IPlasaView.PlasaData({
@@ -115,6 +41,43 @@ contract Plasa is Ownable, IPlasaView {
 		});
 
 		return
-			IPlasaView.PlasaView(data, IPlasaView.PlasaUser({ username: "testusername" }), stampsViews, spacesPreviews);
+			IPlasaView.PlasaView({
+				data: data,
+				user: IPlasaView.PlasaUser({ username: "testusername" }),
+				stamps: stampsViews,
+				spaces: spacesPreviews
+			});
+	}
+
+	/// @inheritdoc IPlasa
+	function addStamp(address stamp) external onlyOwner {
+		_stamps.push(IStamp(stamp));
+	}
+
+	/// @inheritdoc IPlasa
+	function addSpace(address space) external onlyOwner {
+		_spaces.push(ISpace(space));
+	}
+
+	/// @inheritdoc IPlasa
+	function getSpaces() external view returns (ISpace[] memory) {
+		return _spaces;
+	}
+
+	/// @inheritdoc IPlasa
+	function getStamps() external view returns (IStamp[] memory) {
+		return _stamps;
+	}
+
+	/// @inheritdoc IPlasa
+	function getSpace(uint256 index) external view returns (ISpace) {
+		require(index < _spaces.length, "Plasa: Space index out of bounds");
+		return _spaces[index];
+	}
+
+	/// @inheritdoc IPlasa
+	function getStamp(uint256 index) external view returns (IStamp) {
+		require(index < _stamps.length, "Plasa: Stamp index out of bounds");
+		return _stamps[index];
 	}
 }

a440c2af2129327b65071b33cf8f84f257d7080c
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-18 19:05:06 -0300

    update deploy scripts

--- script/deploy/DeployPoC.s.sol
+++ script/deploy/DeployPoC.s.sol
@@ -8,6 +8,9 @@ import { DeployPoCArgs } from "./DeployPoCArgs.s.sol";
 import { IFollowerSinceStamp } from "../../src/stamps/interfaces/IFollowerSinceStamp.sol";
 import { IFollowerSincePoints } from "../../src/points/interfaces/IFollowerSincePoints.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import { FollowerSinceStamp } from "../../src/stamps/FollowerSinceStamp.sol";
+import { FollowerSincePoints } from "../../src/points/FollowerSincePoints.sol";
+import { Plasa } from "../../src/plasa/Plasa.sol";
 
 contract DeployPoC is Script {
 	function run() public {
@@ -22,26 +25,29 @@ contract DeployPoC is Script {
 
 		vm.startBroadcast(deployerPrivateKey);
 
-		// Deploy Space
+		FollowerSinceStamp stamp = new FollowerSinceStamp(args.stampSigner, args.stampPlatform, args.stampFollowed);
+
+		FollowerSincePoints points = new FollowerSincePoints(address(stamp), args.pointsName, args.pointsSymbol);
+
 		Space space = new Space(
 			args.initialSuperAdmins,
 			args.initialAdmins,
 			args.initialModerators,
-			args.stampSigner,
-			args.stampPlatform,
-			args.stampFollowed,
 			args.spaceName,
 			args.spaceDescription,
 			args.spaceImageUrl,
-			args.pointsSymbol,
+			address(points),
 			args.minPointsToAddOpenQuestionOption
 		);
 
-		vm.stopBroadcast();
+		// Deploy Plasa
+		// Set space initial super admin as Plasa owner
+		Plasa plasa = new Plasa(args.initialSuperAdmins[0]);
 
-		// Retrieve Stamp and Points contract addresses
-		IFollowerSinceStamp stamp = space.followerStamp();
-		IFollowerSincePoints points = space.defaultPoints();
+		plasa.addStamp(address(stamp));
+		plasa.addSpace(address(space));
+
+		vm.stopBroadcast();
 
 		// Log deployment information in JSON format
 		console.log("DEPLOYMENT_LOG_START");
@@ -50,6 +56,32 @@ contract DeployPoC is Script {
 		console.log('  "ChainId": %d,', block.chainid);
 		// console.log('  "CompilerVersion": "%s",', vm.envString("FOUNDRY_SOLC_VERSION"));
 		console.log('  "Contracts": [');
+		console.log("    {");
+		console.log('      "FollowerSinceStamp": {');
+		console.log('        "address": "%s",', address(stamp));
+		console.log('        "deployer": "%s",', deployer);
+		console.log('        "contractName": "FollowerSinceStamp",');
+		console.log('        "sourcePath": "src/stamps/FollowerSinceStamp.sol",');
+		console.log('        "arguments": {');
+		console.log('          "signer": "%s",', args.stampSigner);
+		console.log('          "platform": "%s",', args.stampPlatform);
+		console.log('          "followed": "%s"', args.stampFollowed);
+		console.log("        }");
+		console.log("      }");
+		console.log("    },");
+		console.log("    {");
+		console.log('      "FollowerSincePoints": {');
+		console.log('        "address": "%s",', address(points));
+		console.log('        "deployer": "%s",', deployer);
+		console.log('        "contractName": "FollowerSincePoints",');
+		console.log('        "sourcePath": "src/points/FollowerSincePoints.sol",');
+		console.log('        "arguments": {');
+		console.log('          "followerStamp": "%s",', address(stamp));
+		console.log('          "name": "%s",', args.pointsName);
+		console.log('          "symbol": "%s"', args.pointsSymbol);
+		console.log("        }");
+		console.log("      }");
+		console.log("    },");
 		console.log("    {");
 		console.log('      "Space": {');
 		console.log('        "address": "%s",', address(space));
@@ -60,39 +92,22 @@ contract DeployPoC is Script {
 		console.log('          "initialSuperAdmins": %s,', _formatAddressArray(args.initialSuperAdmins));
 		console.log('          "initialAdmins": %s,', _formatAddressArray(args.initialAdmins));
 		console.log('          "initialModerators": %s,', _formatAddressArray(args.initialModerators));
-		console.log('          "stampSigner": "%s",', args.stampSigner);
-		console.log('          "platform": "%s",', args.stampPlatform);
-		console.log('          "followed": "%s",', args.stampFollowed);
 		console.log('          "spaceName": "%s",', args.spaceName);
 		console.log('          "spaceDescription": "%s",', args.spaceDescription);
 		console.log('          "spaceImageUrl": "%s",', args.spaceImageUrl);
-		console.log('          "pointsSymbol": "%s"', args.pointsSymbol);
+		console.log('          "defaultPoints": "%s",', address(points));
+		console.log('          "minPointsToAddOpenQuestionOption": %d', args.minPointsToAddOpenQuestionOption);
 		console.log("        }");
 		console.log("      }");
 		console.log("    },");
 		console.log("    {");
-		console.log('      "FollowerSinceStamp": {');
-		console.log('        "address": "%s",', address(stamp));
-		console.log('        "deployer": "%s",', address(space));
-		console.log('        "contractName": "FollowerSinceStamp",');
-		console.log('        "sourcePath": "src/stamps/FollowerSinceStamp.sol",');
+		console.log('      "Plasa": {');
+		console.log('        "address": "%s",', address(plasa));
+		console.log('        "deployer": "%s",', deployer);
+		console.log('        "contractName": "Plasa",');
+		console.log('        "sourcePath": "src/plasa/Plasa.sol",');
 		console.log('        "arguments": {');
-		console.log('          "signer": "%s",', args.stampSigner);
-		console.log('          "platform": "%s",', stamp.PLATFORM());
-		console.log('          "followed": "%s"', stamp.FOLLOWED());
-		console.log("        }");
-		console.log("      }");
-		console.log("    },");
-		console.log("    {");
-		console.log('      "FollowerSincePoints": {');
-		console.log('        "address": "%s",', address(points));
-		console.log('        "deployer": "%s",', address(space));
-		console.log('        "contractName": "FollowerSincePoints",');
-		console.log('        "sourcePath": "src/points/FollowerSincePoints.sol",');
-		console.log('        "arguments": {');
-		console.log('          "followerStamp": "%s",', address(stamp));
-		console.log('          "name": "%s",', points.name());
-		console.log('          "symbol": "%s"', points.symbol());
+		console.log('          "initialOwner": "%s"', args.initialSuperAdmins[0]);
 		console.log("        }");
 		console.log("      }");
 		console.log("    }");
--- script/deploy/DeployPoCArgs.s.sol
+++ script/deploy/DeployPoCArgs.s.sol
@@ -5,17 +5,20 @@ import { Script } from "forge-std/Script.sol";
 
 contract DeployPoCArgs is Script {
 	struct DeploymentArgs {
+		// Stamp args
+		address stampSigner;
+		string stampPlatform;
+		string stampFollowed;
+		// Points args
+		string pointsName;
+		string pointsSymbol;
 		// Space args
 		address[] initialSuperAdmins;
 		address[] initialAdmins;
 		address[] initialModerators;
-		address stampSigner;
-		string stampPlatform;
-		string stampFollowed;
 		string spaceName;
 		string spaceDescription;
 		string spaceImageUrl;
-		string pointsSymbol;
 		uint256 minPointsToAddOpenQuestionOption;
 	}
 
@@ -33,16 +36,20 @@ contract DeployPoCArgs is Script {
 		// modulators[0] = vm.envAddress("MODERATOR_ADDRESS");
 
 		DeploymentArgs memory args = DeploymentArgs({
-			initialSuperAdmins: superAdmins,
-			initialAdmins: new address[](0),
-			initialModerators: new address[](0),
+			// Stamp args
 			stampSigner: vm.envAddress("EIP712_SIGNER_ADDRESS"),
 			stampPlatform: "Instagram",
 			stampFollowed: "base_onchain",
+			// Points args
+			pointsName: "Base Onchain Points",
+			pointsSymbol: "ONCHAIN",
+			// Space args
+			initialSuperAdmins: superAdmins,
+			initialAdmins: new address[](0),
+			initialModerators: new address[](0),
 			spaceName: "Base",
 			spaceDescription: "The (un)official community space for Base Onchain community",
 			spaceImageUrl: "https://raw.githubusercontent.com/base-org/brand-kit/refs/heads/main/logo/in-product/Base_Network_Logo.png",
-			pointsSymbol: "BASE",
 			minPointsToAddOpenQuestionOption: 100
 		});
 

fa3d4bfd7864f4575b61ad26ba2c827ffaadc52b
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-20 19:43:11 -0300

    deploy: update scripts

--- script/deploy/DeployPoC.s.sol
+++ script/deploy/DeployPoC.s.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.20;
 import { Script, console } from "forge-std/Script.sol";
 import { Space } from "../../src/spaces/Space.sol";
 import { ISpace } from "../../src/spaces/interfaces/ISpace.sol";
-import { DeployPoCArgs } from "./DeployPoCArgs.s.sol";
+import { DeployPoCArgs } from "./DeployPoCArgs.sol";
 import { IFollowerSinceStamp } from "../../src/stamps/interfaces/IFollowerSinceStamp.sol";
 import { IFollowerSincePoints } from "../../src/points/interfaces/IFollowerSincePoints.sol";
+import { IMultipleFollowerSincePoints } from "../../src/points/interfaces/IMultipleFollowerSincePoints.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import { FollowerSinceStamp } from "../../src/stamps/FollowerSinceStamp.sol";
 import { FollowerSincePoints } from "../../src/points/FollowerSincePoints.sol";
+import { MultipleFollowerSincePoints } from "../../src/points/MultipleFollowerSincePoints.sol";
 import { Plasa } from "../../src/plasa/Plasa.sol";
 import { FixedQuestion } from "../../src/voting/FixedQuestion.sol";
 
@@ -17,7 +19,6 @@ contract DeployPoC is Script {
 	function run() public {
 		uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-		// Create an instance of DeployPoCArgs with the deployer's address
 		address deployer = vm.addr(deployerPrivateKey);
 		console.log("Deployer address:", deployer);
 
@@ -26,47 +27,81 @@ contract DeployPoC is Script {
 
 		vm.startBroadcast(deployerPrivateKey);
 
-		FollowerSinceStamp stamp = new FollowerSinceStamp(args.stampSigner, args.stampPlatform, args.stampFollowed);
+		// Deploy Plasa
+		Plasa plasa = new Plasa(args.initialSuperAdmins[0]);
 
-		FollowerSincePoints points = new FollowerSincePoints(address(stamp), args.pointsName, args.pointsSymbol);
-
-		Space space = new Space(
+		// Deploy Space 1 with FollowerSincePoints
+		FollowerSinceStamp stamp1 = new FollowerSinceStamp(args.stampSigner, args.stamp1Platform, args.stamp1Followed);
+		FollowerSincePoints points1 = new FollowerSincePoints(address(stamp1), args.points1Name, args.points1Symbol);
+		Space space1 = new Space(
 			args.initialSuperAdmins,
-			args.spaceName,
-			args.spaceDescription,
-			args.spaceImageUrl,
-			address(points),
+			args.space1Name,
+			args.space1Description,
+			args.space1ImageUrl,
+			address(points1),
 			args.minPointsToAddOpenQuestionOption
 		);
 
-		// Deploy Plasa
-		// Set space initial super admin as Plasa owner
-		Plasa plasa = new Plasa(args.initialSuperAdmins[0]);
-
-		// Stop the broadcast with the deployer's private key
-		vm.stopBroadcast();
-
-		// Start a new broadcast with the initial super admin's private key
-		uint256 superAdminPrivateKey = vm.envUint("SUPER_ADMIN_PRIVATE_KEY");
-		vm.startBroadcast(superAdminPrivateKey);
-
-		plasa.addStamp(address(stamp));
-		plasa.addSpace(address(space));
-
-		// Deploy a fixed question using the super admin's private key
-		FixedQuestion fixedQuestion = new FixedQuestion(
-			address(space),
-			args.fixedQuestionTitle,
-			args.fixedQuestionDescription,
-			args.fixedQuestionDeadline,
-			args.fixedQuestionOptionTitles,
-			args.fixedQuestionOptionDescriptions
+		// Deploy Space 2 with MultipleFollowerSincePoints
+		FollowerSinceStamp[] memory stamps2 = new FollowerSinceStamp[](args.stamp2Platforms.length);
+		address[] memory stampAddresses2 = new address[](args.stamp2Platforms.length);
+		for (uint256 i = 0; i < args.stamp2Platforms.length; i++) {
+			stamps2[i] = new FollowerSinceStamp(args.stampSigner, args.stamp2Platforms[i], args.stamp2Followed[i]);
+			stampAddresses2[i] = address(stamps2[i]);
+		}
+		MultipleFollowerSincePoints points2 = new MultipleFollowerSincePoints(
+			stampAddresses2,
+			args.stamp2Multipliers,
+			args.points2Name,
+			args.points2Symbol
+		);
+		Space space2 = new Space(
+			args.initialSuperAdmins,
+			args.space2Name,
+			args.space2Description,
+			args.space2ImageUrl,
+			address(points2),
+			args.minPointsToAddOpenQuestionOption
 		);
 
-		// Add the question to the space
-		space.addQuestion(address(fixedQuestion));
+		// Deploy question for Space 1
+		FixedQuestion question1 = new FixedQuestion(
+			address(space1),
+			args.question1Title,
+			args.question1Description,
+			args.question1Deadline,
+			args.question1OptionTitles,
+			args.question1OptionDescriptions
+		);
 
-		// Stop the broadcast with the super admin's private key
+		// Deploy question for Space 2
+		FixedQuestion question2 = new FixedQuestion(
+			address(space2),
+			args.question2Title,
+			args.question2Description,
+			args.question2Deadline,
+			args.question2OptionTitles,
+			args.question2OptionDescriptions
+		);
+
+		// Add stamps, spaces, and questions to Plasa and respective spaces
+		plasa.addStamp(address(stamp1));
+		plasa.addSpace(address(space1));
+		space1.addQuestion(address(question1));
+
+		for (uint256 i = 0; i < stamps2.length; i++) {
+			plasa.addStamp(address(stamps2[i]));
+		}
+		plasa.addSpace(address(space2));
+		space2.addQuestion(address(question2));
+
 		vm.stopBroadcast();
+
+		// Log deployed contract addresses
+		console.log("Plasa deployed at:", address(plasa));
+		console.log("Space 1 deployed at:", address(space1));
+		console.log("Space 2 deployed at:", address(space2));
+		console.log("Question 1 deployed at:", address(question1));
+		console.log("Question 2 deployed at:", address(question2));
 	}
 }

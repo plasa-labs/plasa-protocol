3130d01b768a2aa408f14de4ccb86806c7e5efcb
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-05 18:41:34 -0300

    feat: update sol version

--- src/names/INames.sol
+++ src/names/INames.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import { IERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 

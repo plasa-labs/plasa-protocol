55b0bf09588c13dab7b644d6ab290dc9aabcc395
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-05 18:57:42 -0300

    feat: create plasa

--- src/plasa/Plasa.sol
+++ src/plasa/Plasa.sol
@@ -1,83 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// Importing necessary contracts and interfaces
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Space, ISpace, ISpaceView } from "../spaces/Space.sol";
-import { AccountOwnershipStamp, IAccountOwnershipStamp } from "../stamps/AccountOwnershipStamp.sol";
-import { IPlasa } from "./IPlasa.sol";
-import { IStamp, IStampView } from "../stamps/interfaces/IStamp.sol";
-import { IPlasaView } from "./IPlasaView.sol";
+import { INames } from "../names/INames.sol";
+import { IPlasa } from "./interfaces/IPlasa.sol";
 
-/// @title Plasa - The main contract for managing spaces and account ownership stamps
-/// @notice This contract serves as the central hub for all Plasa-related operations
-/// @dev Inherits from Ownable for access control and implements IPlasa interface
+/**
+ * @title Plasa Contract
+ * @dev This contract manages user names and their registration status.
+ * It is owned by a single entity that can update the names contract.
+ */
 contract Plasa is Ownable, IPlasa {
-	/// @notice Array to store all created spaces
-	ISpace[] private _spaces;
+	// Instance of the INames contract to manage user names
+	INames public names;
 
-	/// @notice Array to store all stamps
-	IStamp[] private _stamps;
-
-	/// @notice Initializes the Plasa contract
-	/// @param initialOwner The address that will own this Plasa contract
-	constructor(address initialOwner) Ownable(initialOwner) {}
-
-	/// @inheritdoc IPlasaView
-	function getPlasaView(address user) external view returns (IPlasaView.PlasaView memory) {
-		IStampView.StampView[] memory stampsViews = new IStampView.StampView[](_stamps.length);
-		for (uint256 i = 0; i < _stamps.length; i++) {
-			stampsViews[i] = _stamps[i].getStampView(user);
-		}
-
-		ISpaceView.SpacePreview[] memory spacesPreviews = new ISpaceView.SpacePreview[](_spaces.length);
-		for (uint256 i = 0; i < _spaces.length; i++) {
-			spacesPreviews[i] = _spaces[i].getSpacePreview(user);
-		}
-
-		IPlasaView.PlasaData memory data = IPlasaView.PlasaData({
-			contractAddress: address(this),
-			chainId: block.chainid,
-			version: "0.1.0"
-		});
-
-		return
-			IPlasaView.PlasaView({
-				data: data,
-				user: IPlasaView.PlasaUser({ username: "testusername" }),
-				stamps: stampsViews,
-				spaces: spacesPreviews
-			});
+	/**
+	 * @dev Constructor to initialize the contract with an owner and names contract address.
+	 * @param _owner The address of the contract owner.
+	 * @param _namesContract The address of the INames contract.
+	 */
+	constructor(address _owner, address _namesContract) Ownable(_owner) {
+		names = INames(_namesContract);
 	}
 
-	/// @inheritdoc IPlasa
-	function addStamp(address stamp) external onlyOwner {
-		_stamps.push(IStamp(stamp));
+	/**
+	 * @dev Updates the address of the names contract.
+	 * @param _namesContract The new address of the INames contract.
+	 */
+	function setNamesContract(address _namesContract) external onlyOwner {
+		names = INames(_namesContract);
 	}
 
-	/// @inheritdoc IPlasa
-	function addSpace(address space) external onlyOwner {
-		_spaces.push(ISpace(space));
+	/**
+	 * @dev Checks if a user is registered based on their balance in the names contract.
+	 * @param user The address of the user to check.
+	 * @return bool indicating whether the user is registered.
+	 */
+	function isRegistered(address user) public view returns (bool) {
+		return names.balanceOf(user) > 0;
 	}
 
-	/// @inheritdoc IPlasa
-	function getSpaces() external view returns (ISpace[] memory) {
-		return _spaces;
-	}
-
-	/// @inheritdoc IPlasa
-	function getStamps() external view returns (IStamp[] memory) {
-		return _stamps;
-	}
-
-	/// @inheritdoc IPlasa
-	function getSpace(uint256 index) external view returns (ISpace) {
-		if (index >= _spaces.length) revert IndexOutOfBounds(index, _spaces.length);
-		return _spaces[index];
-	}
-
-	/// @inheritdoc IPlasa
-	function getStamp(uint256 index) external view returns (IStamp) {
-		if (index >= _stamps.length) revert IndexOutOfBounds(index, _stamps.length);
-		return _stamps[index];
+	/**
+	 * @dev Retrieves the username associated with a given user address.
+	 * @param user The address of the user whose username is to be retrieved.
+	 * @return string memory The username of the user.
+	 */
+	function getUsername(address user) public view returns (string memory) {
+		return names.userToName(user);
 	}
 }
